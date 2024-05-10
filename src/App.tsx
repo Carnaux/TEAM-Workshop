b2ec6ebb50eb72d033811be5d3fdde53a99de4b9
Daniel Fernandes <danielfeenandes@gmail.com>
2024-05-10 03:54:06 -0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import './App.css';
-import Box from './components/Box';
-import { OrbitControls, Stats } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { useState } from "react";
+import "./App.css";
+import { Bike } from "./components/Bike";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-
+  const [selectedPart, setSelectedPart] = useState<string>("");
 
   return (
     <>
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <Stats />
-    </Canvas>
+      <Canvas shadows camera={{ position: [0, 0, 3.5], fov: 35 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight
+          intensity={0.5}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, -5]}
+          castShadow
+        />
+        <Environment preset="city" background blur={1} />
+        <ambientLight />
+        <OrbitControls
+          makeDefault
+          enabled={selectedPart !== "" ? false : true}
+          enableDamping={true}
+          autoRotate={selectedPart !== "" ? false : true}
+          autoRotateSpeed={0.5}
+          enableZoom={false}
+          enablePan={false}
+          rotateSpeed={0.5}
+          minPolarAngle={Math.PI / 4}
+          maxPolarAngle={Math.PI / 2}
+        />
+        <ContactShadows
+          resolution={512}
+          position={[0, -0.6, 0]}
+          opacity={1}
+          scale={10}
+          blur={2}
+          far={0.8}
+        />
+        <Bike
+          position={[0, 0, 0]}
+          rotation={[0, Math.PI / 2, 0]}
+          selectedPart={selectedPart}
+          setSelectedPart={setSelectedPart}
+        />
+      </Canvas>
     </>
   );
 }
---
+++ src/components/Bike.tsx
@@ -0,0 +1,88 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { HoverOpacityEnter, HoverOpacityLeave } from "../utils/HoverOpacity";
+
+export const Bike = (props: any) => {
+  const ref = useRef(null);
+  const gltf = useLoader(GLTFLoader, "/bike.glb");
+
+  useEffect(() => {
+    if (ref.current) {
+      gltf.scene.traverse((child) => {
+        if (child instanceof Mesh) {
+          child.material.transparent = true;
+          child.material.needsUpdate = true;
+        }
+      });
+    }
+  }, [ref.current]);
+
+  const handleMouseOver = (e: any) => {
+    e.stopPropagation();
+    const objName = e.object.name;
+
+    if (props.selectedPart !== "") return;
+
+    if (objName.includes("Garfo")) {
+      HoverOpacityEnter(gltf, "Garfo");
+    } else if (objName.includes("Gearbox")) {
+      HoverOpacityEnter(gltf, "Gearbox");
+    } else if (objName.includes("mola")) {
+      HoverOpacityEnter(gltf, "mola");
+    } else if (objName.includes("Quadro")) {
+      HoverOpacityEnter(gltf, "Quadro");
+    }
+  };
+
+  const handleMouseLeave = (e: any) => {
+    e.stopPropagation();
+
+    if (props.selectedPart !== "") return;
+
+    const objName = e.object.name;
+
+    if (objName.includes("Garfo")) {
+      HoverOpacityLeave(gltf, "Garfo");
+    } else if (objName.includes("Gearbox")) {
+      HoverOpacityLeave(gltf, "Gearbox");
+    } else if (objName.includes("mola")) {
+      HoverOpacityLeave(gltf, "mola");
+    } else if (objName.includes("Quadro")) {
+      HoverOpacityLeave(gltf, "Quadro");
+    }
+  };
+
+  const handleMouseClick = (e: any) => {
+    e.stopPropagation();
+    const objName = e.object.name;
+
+    if (objName.includes("Garfo")) {
+      props.setSelectedPart("Garfo");
+    } else if (objName.includes("Gearbox")) {
+      props.setSelectedPart("Gearbox");
+    } else if (objName.includes("mola")) {
+      props.setSelectedPart("mola");
+    } else if (objName.includes("Quadro")) {
+      props.setSelectedPart("Quadro");
+    }
+  };
+
+  return (
+    <primitive
+      ref={ref}
+      object={gltf.scene}
+      {...props}
+      onPointerEnter={(e: any) => {
+        handleMouseOver(e);
+      }}
+      onPointerOut={(e: any) => {
+        handleMouseLeave(e);
+      }}
+      onClick={(e: any) => {
+        handleMouseClick(e);
+      }}
+    />
+  );
+};
